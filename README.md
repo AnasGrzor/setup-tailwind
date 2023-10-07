bba80548464d74763f0e40221bf7e0f244e8b162
AnasGrzor <anaskhalid658@gmail.com>
2023-10-08 01:32:27 +0500

    updated README

---
+++ README.md
@@ -0,0 +1,39 @@
+# Setup Tailwind CSS Package
+
+This npm package automates the setup of a [Tailwind CSS](https://tailwindcss.com/) environment with [Vite](https://vitejs.dev/). It streamlines the process of getting started with Tailwind CSS in your project by installing the necessary dependencies, initializing Tailwind CSS, and configuring a basic project structure.
+
+## Installation
+
+To use this package, you can install it globally using npm:
+
+```bash
+npm install -g setup-tailwind-vanilla
+```
+
+Usage
+After installing the package, you can run the setup command in your project directory:
+
+```bash
+npm setup-tailwind
+```
+
+Features
+Automated Setup: Quickly set up a Tailwind CSS project with minimal manual configuration.
+Tailwind Configuration: The package updates the Tailwind CSS configuration to include all classes by default.
+Vite Integration: Vite is used as the build tool for a fast development experience.
+Configuration
+You can customize the behavior of this package by modifying the setup-tailwind.js script included in your project.
+
+License
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+Acknowledgments
+Tailwind CSS
+Vite
+Contributing
+Contributions are welcome! Feel free to open an issue or create a pull request.
+
+Authors
+[AnasGrzor]
+Contact
+If you have any questions or feedback, you can reach out to [anaskhalid658@gmail.com].
---
+++ setup-tailwind.js
@@ -0,0 +1,56 @@
+const { execSync, writeFileSync } = require("child_process");
+const fs = require("fs");
+const readline = require("readline");
+
+// Create a readline interface to get user input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+// Run setup commands
+execSync("npm install -D tailwindcss postcss autoprefixer vite", {
+  stdio: "inherit",
+});
+execSync("npx tailwindcss init -p", { stdio: "inherit" }); // Added -p flag for parallel mode
+
+// Update the content property in tailwind.config.js to ["*"]
+const configPath = "tailwind.config.js";
+const configContent = fs.readFileSync(configPath, "utf8");
+const updatedConfigContent = configContent.replace(
+  /content: \[\]/,
+  'content: ["*"]'
+);
+fs.writeFileSync(configPath, updatedConfigContent);
+
+// Create input.css file
+const cssContent = `@tailwind base;\n@tailwind components;\n@tailwind utilities;`;
+fs.writeFileSync("input.css", cssContent);
+
+// Check if package.json exists, and if not, create it
+if (!fs.existsSync("package.json")) {
+  const newPackageJson = {
+    name: "my-project", // Set your project name
+    version: "1.0.0",
+    scripts: {
+      start: "vite",
+    },
+    devDependencies: {},
+  };
+  fs.writeFileSync("package.json", JSON.stringify(newPackageJson, null, 2));
+} else {
+  // If package.json exists, ensure it has a "scripts" section with a "start" script
+  const packageJson = JSON.parse(fs.readFileSync("package.json", "utf8"));
+  if (!packageJson.scripts || !packageJson.scripts.start) {
+    packageJson.scripts = {
+      start: "vite",
+    };
+    fs.writeFileSync("package.json", JSON.stringify(packageJson, null, 2));
+  }
+}
+
+console.log(
+  "Tailwind CSS setup complete with updated content property and parallel Vite execution!"
+);
+
+// Close the readline interface
+rl.close();
